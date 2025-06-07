8445eb7d52e221b3793d9538c4a17a08c8ddcfba
Emilly Costa <166616351+emmycosta@users.noreply.github.com>
2025-06-07 19:39:12 -0300

    Update home.blade.php

--- Agenda_Beauty/resources/views/home.blade.php
+++ Agenda_Beauty/resources/views/home.blade.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HOME AGENDA BEAUTY</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Bem-vindo ao Agenda Beauty</h1>
-    <p>Agenda Beauty é uma plataforma de agendamento de serviços de beleza.</p>
+  <div class="container">
+    <div class="left-side">
+      <img src="imagens/AgendaBeauty.png" alt="Logo Agenda Beauty" class="logo-ab">
+    </div>
+    <div class="right-side">
+  <div class="lotus-wrapper">
+    <img src="imagens/florLotus.png" alt="Flor de Lótus" class="logo-lotus">
+  </div>
+  <div class="botoes">
+    <a href="login.html"><button class="btn">Fazer login</button></a>
+    <a href="cadastro.html"><button class="btn">Cadastre-se</button></a>
+  </div>
+</div>
+    </div>
+  </div>
 </body>
 </html>
